cd0e6dfd0eda973866e41bcbc2436431b3f4bba2
Jordan <jordanjohnso2021@fau.edu>
2026-01-17 18:46:09 -0500

    Use template literal to render game info and image in game card

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ function addGamesToPage(games) {
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
+        element.innerHTML = `
+            <img class="game-img" src="${game.img}" />
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+        `;
 
         // append the game to the games-container
 
