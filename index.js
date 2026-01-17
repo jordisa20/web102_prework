50519f9e5ee54683f4fcb136372aa5e50082dbb5
Jordan <jordanjohnso2021@fau.edu>
2026-01-17 18:56:48 -0500

    Add event listeners to filter buttons for game display

--- index.js
+++ index.js
@@ -136,6 +136,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 
 /*************************************************************************************
